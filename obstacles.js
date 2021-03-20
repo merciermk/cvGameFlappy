3a66fee610623e08fdac5f6b84a26820a11f0880
Mike <merciermk@free.fr>
2021-03-20 12:27:44 +0100

    randomisation obstacles et radomisation y des obstacles

--- obstacles.js
+++ obstacles.js
@@ -1,20 +1,26 @@
 // 200*850
+var obstacleImg =[]
+obstacleImg[0] = new Image()
+obstacleImg[0].src = "./img/obstacles/obstacle1.png"
 
-var obstacleImg = new Image()
-obstacleImg.src = "./img/obstacles/obstacle1.png"
+obstacleImg[1] = new Image()
+obstacleImg[1].src = "./img/obstacles/obstacle2.png"
 
+obstacleImg[2] = new Image()
+obstacleImg[2].src = "./img/obstacles/obstacle1.png"
 
 var ObstacleFactory = function(){
+    this.imgNumber = Math.floor(Math.random()*2)
     this.sx = 0;
     this.sy = 0;
     this.swidth = 200;
     this.sheight = 850;
     this.dx = 600;
-    this.dy = 0;
+    this.dy = Math.floor(Math.random()*-100);
     this.dwidth = this.swidth/5;
     this.dheight = this.sheight/5;
     this.draw = function(){
-        ctx.drawImage(obstacleImg,this.sx,this.sy,this.swidth,this.sheight,this.dx,this.dy,this.dwidth,this.dheight)
+        ctx.drawImage(obstacleImg[this.imgNumber],this.sx,this.sy,this.swidth,this.sheight,this.dx,this.dy,this.dwidth,this.dheight)
     }
     this.frame = () =>{
         this.dx -= 1;
